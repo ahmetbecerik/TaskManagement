a2d5b3d6e33371ea9471daaac28b444d14c6579c
ahmetbecerik <ahmet@becerik.com>
2021-08-12 17:01:20 +0300

    Gereksiz bosluklar silindi.

--- src/main/java/com/example/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/example/taskmanagement/TaskManagementApplication.java
@@ -31,11 +31,6 @@ public class TaskManagementApplication {
         List<Card> cards2 = Arrays.asList(card2, card3);
         board2.setCards(cards2);
         boardRepository.save(board2);
-
-
-
-
-
     }
 
 }
